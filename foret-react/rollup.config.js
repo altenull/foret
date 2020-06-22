db92a9c37e83ff725e46e87a3d335f1fdbe14691
Heonyoung Kim <altenull@gmail.com>
2020-06-22 14:28:56 +0900

    build: resolve @rollup/plugin-typescript issue by replacing it to rollup-plugin-typescript2

--- foret-react/rollup.config.js
+++ foret-react/rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import babel from '@rollup/plugin-babel';
 
