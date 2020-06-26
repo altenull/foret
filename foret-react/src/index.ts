5ecd5a0e8cd494a9e45efbf4d0da1a55b327f7c9
Heonyoung Kim <altenull@gmail.com>
2020-06-26 14:14:13 +0900

    feat(react): add color values

--- foret-react/src/index.ts
+++ foret-react/src/index.ts
@@ -1,3 +1,5 @@
 import { Button } from './lib/components/Button/Button';
+import { Heading1, Heading2, Heading3, Paragraph } from './lib/typography';
 
 export { Button };
+export { Heading1, Heading2, Heading3, Paragraph };
---
+++ foret-react/src/lib/utils/color.utils.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  foretGreen: '#2e8b57',
+  soil: '#503715',
+  ink: '#262626',
+  black: '#000000',
+  white: '#ffffff',
+  danger: '#de2e21',
+  warning: '#ffa126',
+  success: '#43c463',
+  information: '#0875e1',
+};
