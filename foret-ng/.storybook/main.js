e041623d762084cb25b1b15d289aa7018b0a312f
Heonyoung Kim <altenull@gmail.com>
2020-07-15 14:09:21 +0900

    fix(ng): being able to have assets instead of @altenull/foret-sass

--- foret-ng/.storybook/main.js
+++ foret-ng/.storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
-  stories: ["../projects/foret-ng/src/lib/**/*.stories.ts"],
-  addons: [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-notes",
-  ],
+  stories: ['../projects/foret-ng/src/lib/**/*.stories.ts'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes'],
 };
