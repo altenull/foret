990d440e981559d0dc2f0c32e65e1b158c9dd2fe
Heonyoung Kim <altenull@gmail.com>
2020-07-18 16:09:14 +0900

    build(ng): add @storybook/theming module and create theme

--- foret-ng/.storybook/preview.js
+++ foret-ng/.storybook/preview.js
@@ -1,9 +1,20 @@
 import { Color } from '@altenull/foret-core';
 import { addParameters } from '@storybook/angular';
+import { create } from '@storybook/theming';
+import { description, version } from '../package.json';
+
+const theme = create({
+  base: 'light',
+  brandTitle: `${description} ${version}`,
+  showPanel: true,
+});
 
 addParameters({
   backgrounds: [
     { name: 'paper', value: Color.Paper, default: true },
     { name: 'white', value: Color.White },
   ],
+  options: {
+    theme,
+  },
 });
