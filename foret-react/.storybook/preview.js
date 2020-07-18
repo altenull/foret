f9186fbc158c07e8613d884f7f5b33ea68daf056
Heonyoung Kim <altenull@gmail.com>
2020-07-18 16:06:48 +0900

    build(react): add @storybook/theming module and create theme

--- foret-react/.storybook/preview.js
+++ foret-react/.storybook/preview.js
@@ -1,9 +1,20 @@
-import { addParameters } from '@storybook/react';
 import { Color } from '@altenull/foret-core';
+import { addParameters } from '@storybook/react';
+import { create } from '@storybook/theming';
+import { description, version } from '../package.json';
+
+const theme = create({
+  base: 'light',
+  brandTitle: `${description} ${version}`,
+  showPanel: true,
+});
 
 addParameters({
   backgrounds: [
     { name: 'paper', value: Color.Paper, default: true },
     { name: 'white', value: Color.White },
   ],
+  options: {
+    theme,
+  },
 });
