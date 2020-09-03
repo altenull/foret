1fa9358c637935a933524ceb50ec1cea61824f2b
Heonyoung Kim <altenull@gmail.com>
2020-09-04 00:33:18 +0900

    feat(react, tab): give animation to tab component

--- foret-react/src/lib/components/Tab/TabGroup.tsx
+++ foret-react/src/lib/components/Tab/TabGroup.tsx
@@ -1,11 +1,12 @@
 /** @jsx jsx */
 import { Color } from '@altenull/foret-core';
 import { css, jsx } from '@emotion/core';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { TabGroupProps } from './models/tab-group-props';
 import Tab from './Tab';
 
 const tabListContainerStyles = css`
+  position: relative;
   list-style: none;
   display: flex;
   margin: 0;
@@ -14,8 +15,37 @@ const tabListContainerStyles = css`
   border-bottom: 1px solid ${Color.Fog};
 `;
 
+const tabIndicatorStyles = (left: number, width: number) => css`
+  position: absolute;
+  left: ${left}px;
+  bottom: 0;
+  min-width: ${width}px;
+  min-height: 2px;
+  background-color: ${Color.ForetGreen};
+  transition: all 0.3s linear;
+`;
+
 const TabGroup: React.FC<TabGroupProps> = ({ children, selectedValue, name, onSelect = () => {} }: TabGroupProps) => {
   const [selected, setSelected] = useState<string>(selectedValue);
+  const [tabIndicatorStatus, setTabIndicatorStatus] = useState<{ left: number; width: number }>(null);
+  const tabListContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (tabListContainerRef != null) {
+      const selectedTabIndex: number = React.Children.toArray(children).findIndex(
+        (child) => React.isValidElement(child) && child.props.value === selected
+      );
+      const tabListContainerDOMRect: DOMRect = tabListContainerRef.current.getBoundingClientRect();
+      const selectedTabDOMRect: DOMRect = tabListContainerRef.current.children[
+        selectedTabIndex
+      ].getBoundingClientRect();
+
+      setTabIndicatorStatus({
+        left: selectedTabDOMRect.left - tabListContainerDOMRect.left,
+        width: selectedTabDOMRect.width,
+      });
+    }
+  }, [children, selected, tabListContainerRef, setTabIndicatorStatus]);
 
   const handleSelect = (newSelection: string, name: string, event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
     if (newSelection !== selected) {
@@ -58,7 +88,12 @@ const TabGroup: React.FC<TabGroupProps> = ({ children, selectedValue, name, onSe
 
   return (
     <div>
-      <ul css={tabListContainerStyles}>{getTabs()}</ul>
+      <ul css={tabListContainerStyles} ref={tabListContainerRef}>
+        {getTabs()}
+        {tabIndicatorStatus != null && (
+          <span css={tabIndicatorStyles(tabIndicatorStatus.left, tabIndicatorStatus.width)} />
+        )}
+      </ul>
       <div>{getContent()}</div>
     </div>
   );
