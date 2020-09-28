879bea2b473475253337bed29d5dcefc3434d6c3
Heonyoung Kim <altenull@gmail.com>
2020-09-28 15:41:36 +0900

    chore(core): delete rgb-color variable file

--- foret-core/src/index.ts
+++ foret-core/src/index.ts
@@ -2,11 +2,8 @@
 export { Color } from "./lib/enums/color.enum";
 export { FontWeight } from "./lib/enums/font-weight.enum";
 
-// Functions
-export { hexToRgb } from "./lib/functions/hexToRgb";
-
-// Varaibles
-export { rgbColor } from "./lib/variables/rgb-color.variable";
-
 // Models
 export { RgbColor } from "./lib/models/rgb-color";
+
+// Functions
+export { hexToRgb } from "./lib/functions/hexToRgb";
