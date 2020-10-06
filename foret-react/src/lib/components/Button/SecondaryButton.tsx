45fbe77183c797972607e12d2ac0c58537ee0295
Heonyoung Kim <altenull@gmail.com>
2020-10-06 22:36:42 +0900

    feat(react, button): change secondary button color to stone color

--- foret-react/src/lib/components/Button/SecondaryButton.tsx
+++ foret-react/src/lib/components/Button/SecondaryButton.tsx
@@ -4,25 +4,25 @@ import { css, jsx } from '@emotion/core';
 import { ButtonProps } from './models/button-props';
 import { baseButtonStyles } from './utils/button.utils';
 
-const soilRgb = hexToRgb(Color.Soil);
+const stoneRgb = hexToRgb(Color.Stone);
 
 const secondaryButtonEffectStyles = css({
   '&:hover': {
-    backgroundColor: Color.SoilDarken1,
+    backgroundColor: Color.StoneDarken1,
   },
   '&:active': {
-    backgroundColor: Color.SoilDarken2,
+    backgroundColor: Color.StoneDarken2,
   },
   '&:focus': {
-    boxShadow: `rgba(${soilRgb.r}, ${soilRgb.g}, ${soilRgb.b}, 0.35) 0 0 0 3px`,
+    boxShadow: `rgba(${stoneRgb.r}, ${stoneRgb.g}, ${stoneRgb.b}, 0.35) 0 0 0 3px`,
   },
   '&:disabled': {
-    backgroundColor: Color.SoilLighten,
+    backgroundColor: Color.StoneLighten,
   },
 });
 
 const secondaryButtonStyles = css(baseButtonStyles, secondaryButtonEffectStyles, {
-  backgroundColor: Color.Soil,
+  backgroundColor: Color.Stone,
   color: Color.White,
 });
 
