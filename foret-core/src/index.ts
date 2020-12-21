1425a1f48f2cdb2669951111c6fd90aaf43d38a9
Heonyoung Kim <altenull@gmail.com>
2020-12-21 21:46:51 +0900

    feat(core): add language-code enum

--- foret-core/src/index.ts
+++ foret-core/src/index.ts
@@ -1,9 +1,10 @@
 // Enums
 export { Color } from "./lib/enums/color.enum";
 export { FontWeight } from "./lib/enums/font-weight.enum";
-
-// Models
-export { RgbColor } from "./lib/models/rgb-color";
+export { LanguageCode } from "./lib/enums/language-code.enum";
 
 // Functions
 export { hexToRgb } from "./lib/functions/hexToRgb";
+
+// Models
+export { RgbColor } from "./lib/models/rgb-color";
---
+++ foret-core/src/lib/enums/language-code.enum.ts
@@ -0,0 +1,9 @@
+/**
+ * LanguageCode follows ISO 639-1 format.
+ * It's added to handle i18n of date picker component both in ng and react.
+ * [ISO 639-1] https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+ */
+export enum LanguageCode {
+  EN = "en",
+  KO = "ko",
+}
