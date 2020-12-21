8bcde9a46680a0b3c7f2313cde503b8dad402a51
Heonyoung Kim <altenull@gmail.com>
2020-12-21 20:49:11 +0900

    chore(react, date-picker): export CalendarDay and CalendarDayOfweek in index.ts

--- foret-react/src/lib/components/DatePicker/index.ts
+++ foret-react/src/lib/components/DatePicker/index.ts
@@ -1,3 +1,5 @@
 export { default as Calendar } from './Calendar';
+export { default as CalendarDay } from './CalendarDay';
+export { default as CalendarDayOfWeek } from './CalendarDayOfWeek';
 export { default as DatePicker } from './DatePicker';
 export { default as MonthNavigator } from './MonthNavigator';
