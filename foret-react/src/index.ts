c5e2d00c64a90623dbbe36e1b851f61665e44abd
Heonyoung Kim <altenull@gmail.com>
2020-12-24 20:48:15 +0900

    chore(react, date-picker): export date-picker ui components

--- foret-react/src/index.ts
+++ foret-react/src/index.ts
@@ -1,18 +1,10 @@
 export { DangerButton, PrimaryButton, SecondaryButton } from './lib/components/Button';
 export { Checkbox, CheckboxGroup } from './lib/components/Checkbox';
+export { Calendar, CalendarDay, CalendarDayOfWeek, DatePicker, MonthNavigator } from './lib/components/DatePicker';
 export { RadioButton, RadioButtonGroup } from './lib/components/RadioButton';
 export { Select, SelectItem, SelectItemGroup } from './lib/components/Select';
 export { Tab, TabGroup } from './lib/components/Tab';
 export { Table, TableBody, TableCell, TableHead, TableRow } from './lib/components/Table';
 export { Toggle } from './lib/components/Toggle';
 export { Foret } from './lib/theme';
-export {
-  Heading1,
-  Heading2,
-  Heading3,
-  Paragraph,
-  SmallText,
-  Subtitle1,
-  Subtitle2
-} from './lib/typography';
-
+export { Heading1, Heading2, Heading3, Paragraph, SmallText, Subtitle1, Subtitle2 } from './lib/typography';
